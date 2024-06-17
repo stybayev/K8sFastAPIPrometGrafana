b6e36f070edd90a4d9c0230d2d9aef9498001cf4
dosxana@gmail.com <dosxana@gmail.com>
2024-06-17 20:44:15 +0500

    Initial commit

---
+++ auth/main.py
@@ -0,0 +1,24 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+
+from auth.core.config import settings
+
+
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     pass
+
+
+app = FastAPI(
+    title=settings.project_name,
+    docs_url="/api/auth/openapi",
+    openapi_url="/api/auth/openapi.json",
+    default_response_class=ORJSONResponse,
+    # lifespan=lifespan
+)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
