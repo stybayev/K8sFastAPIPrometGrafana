61ff698e86908840edcaf8e0e52d30eb257701b3
dosxana@gmail.com <dosxana@gmail.com>
2024-06-18 14:44:17 +0500

    Implemented a router for user registration

---
+++ auth/services/users.py
@@ -0,0 +1,35 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from functools import lru_cache
+from typing import Optional
+from fastapi import Depends
+from auth.db.postgres import get_db_session
+from auth.models.users import User
+
+
+class UserService:
+    def __init__(self, db_session: AsyncSession):
+        self.db_session = db_session
+
+    async def get_by_login(self, login: str) -> Optional[User]:
+        """
+        Поиск пользователя по логину
+        """
+        result = await self.db_session.execute(select(User).where(User.login == login))
+        return result.scalar_one_or_none()
+
+    async def create_user(self, login: str, password: str, first_name: Optional[str] = None,
+                          last_name: Optional[str] = None) -> User:
+        """
+        Создание пользователя
+        """
+        new_user = User(login=login, password=password, first_name=first_name, last_name=last_name)
+        self.db_session.add(new_user)
+        await self.db_session.commit()
+        await self.db_session.refresh(new_user)
+        return new_user
+
+
+@lru_cache()
+def get_user_service(db_session: AsyncSession = Depends(get_db_session)) -> UserService:
+    return UserService(db_session)
