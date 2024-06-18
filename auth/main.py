4d9c0c77632cde7267f06cd7a6511d2c03aaecc3
dosxana@gmail.com <dosxana@gmail.com>
2024-06-18 11:49:00 +0500

    Initial commit

--- auth/main.py
+++ auth/main.py
@@ -1,15 +1,22 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
-
+from redis.asyncio import Redis
 from auth.core.config import settings
 from auth.db.postgres import create_database
+from auth.db.redis import redis
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    redis.redis = Redis(host=settings.redis_host, port=settings.redis_port)
+
+    # TODO: создание БД надо будет переделать через alembic
     await create_database()
+
     yield
+
+    await redis.redis.close()
 
 
 app = FastAPI(
