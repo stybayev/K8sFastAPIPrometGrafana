1f9243a95280af6d1dc1a6bfd9f9f6e747b56889
kirill <domitin@gmail.com>
2024-06-20 03:24:58 +0300

    добавил создание, удаление, редактирование и получение всех ролей

--- auth/api/v1/roles.py
+++ auth/api/v1/roles.py
@@ -2,42 +2,58 @@ from typing import List
 from fastapi import APIRouter, Depends, Path, HTTPException, Query, status
 from uuid import UUID
 from auth.models.users import Role
-from auth.schema.roles import RoleSchema
+from auth.schema.roles import RoleSchema, RoleResponse, RoleUpdateSchema
 from auth.services.roles import RoleService, get_role_service
 
 router = APIRouter()
 
 
-@router.post("/roles", response_model=dict)
-async def create_role():
+@router.post("/", response_model=dict)
+async def create_role(role: RoleSchema, service: RoleService = Depends(get_role_service)) -> RoleResponse:
     """
     Создание роли
     """
-    return {'123': 123}
+    new_role = await service.create_role(role)
+    return RoleResponse.from_orm(new_role)
 
 
-# @router.delete("/{role_id}", response_model=Role)
-# async def delete_role(role_id: UUID = Path(..., description="Role ID")):
-#     """
-#     Удаление роли
-#     """
-#     pass
-#
-#
-# @router.patch("/{role_id}", response_model=Role)
-# async def update_role(role_id: UUID = Path(..., description="Role ID")):
-#     """
-#     Изменение роли
-#     """
-#     pass
-#
-#
-# @router.get("/", response_model=List[Role])
-# async def get_roles():
-#     """
-#     Просмотр всех ролей
-#     """
-#     pass
+@router.delete("/")
+async def delete_role(role_id: UUID = None, role_name: str = None,
+                      service: RoleService = Depends(get_role_service)) -> dict:
+    """
+    Удалить роль по ID или имени.
+
+    Параметры:
+    - role_id: UUID (необязательно) - ID роли, которую нужно удалить.
+    - name: str (необязательно) - Имя роли, которую нужно удалить.
+
+    Необходимо указать либо role_id, либо name.
+    """
+    result = await service.delete_role(role_id, role_name)
+    return result
+
+
+@router.patch("/{role_id}")
+async def update_role(role_id: UUID, data: RoleUpdateSchema,
+                      service: RoleService = Depends(get_role_service)) -> RoleResponse:
+    """
+    Обновить роль по ID.
+
+    Параметры:
+    - role_id: UUID - ID роли, которую нужно обновить.
+    - data: RoleUpdateSchema - Данные для обновления.
+    """
+    updated_role = await service.update_role(role_id, data)
+    return RoleResponse.from_orm(updated_role)
+
+
+@router.get("/")
+async def get_roles(service: RoleService = Depends(get_role_service)):
+    """
+    Просмотр всех ролей
+    """
+    roles = await service.get_all_roles()
+    return roles
 #
 #
 # @router.post("/users/{user_id}/roles/{role_id}", response_model=dict)
