8fc6f2b1f12638b24e68fb8d28117c12f141c8ec
dosxana@gmail.com <dosxana@gmail.com>
2024-06-26 13:01:53 +0500

    Added fixes to the migration

--- alembic/versions/3df764df8196_create_users_table.py
+++ alembic/versions/3df764df8196_create_users_table.py
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
-revision: str = '70a851e24518'
+revision: str = '3df764df8196'
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
