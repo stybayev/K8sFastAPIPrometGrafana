9c2ef9c59f0040eacb19cf939a40fa0ab6b66c05
dosxana@gmail.com <dosxana@gmail.com>
2024-08-06 12:45:23 +0500

    Создал файл config.py для хранения конфигов проекта

--- ugc_service/core/config.py
+++ ugc_service/core/config.py
@@ -1,15 +1,16 @@
-# import os
-# from pydantic import BaseSettings, Field
-#
-#
-# class Settings(BaseSettings):
-#     # App
-#     project_name: str = Field(default="UGC Service", env="UGC_SERVICE_NAME")
-#     uvicorn_host: str = Field(default="0.0.0.0", env="UGC_SERVICE_UVICORN_HOST")
-#     uvicorn_port: int = Field(default=8084, env="UGC_SERVICE_UVICORN_PORT")
-#
-#     class Config:
-#         env_file = ".env"
-#
-#
-# settings = Settings()
+import os
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    # App
+    project_name: str = Field(default="UGC Service", env="UGC_SERVICE_NAME")
+    uvicorn_host: str = Field(default="0.0.0.0", env="UGC_SERVICE_UVICORN_HOST")
+    uvicorn_port: int = Field(default=8084, env="UGC_SERVICE_UVICORN_PORT")
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
--- ugc_service/main.py
+++ ugc_service/main.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from core.config import settings
 app = Flask(__name__)
 
 
 @app.route('/hello-world')
 def hello_world():
+    print('Hello, World!')
+    print(settings.project_name)
     return 'Hello, World!'
