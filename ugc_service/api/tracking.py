ff72e64dae7b783bd6d828103bf26bdfbc68899b
dosxana@gmail.com <dosxana@gmail.com>
2024-08-07 12:52:47 +0500

    Добавил автодокументирование для сервиса ugc

--- ugc_service/api/tracking.py
+++ ugc_service/api/tracking.py
@@ -1,15 +1,54 @@
 from flask import Blueprint, request, jsonify
+from flasgger import swag_from
 from core.config import settings
 
 api = Blueprint('api', __name__)
 
-
 @api.route('/hello-world', methods=['GET'])
+@swag_from({
+    'responses': {
+        200: {
+            'description': 'Returns a greeting message',
+            'schema': {
+                'type': 'object',
+                'properties': {
+                    'message': {
+                        'type': 'string',
+                        'example': 'Hello, World!'
+                    }
+                }
+            }
+        }
+    }
+})
 def hello_world():
     return jsonify({"message": "Hello, World!"})
 
-
 @api.route('/config', methods=['GET'])
+@swag_from({
+    'responses': {
+        200: {
+            'description': 'Returns service configuration',
+            'schema': {
+                'type': 'object',
+                'properties': {
+                    'service_name': {
+                        'type': 'string',
+                        'example': 'my_service'
+                    },
+                    'service_uvicorn_host': {
+                        'type': 'string',
+                        'example': '127.0.0.1'
+                    },
+                    'service_uvicorn_port': {
+                        'type': 'integer',
+                        'example': 8000
+                    }
+                }
+            }
+        }
+    }
+})
 def get_config():
     return jsonify({
         "service_name": settings.service_name,
--- ugc_service/main.py
+++ ugc_service/main.py
@@ -1,6 +1,16 @@
 from flask import Flask
 from api.tracking import api
+from flasgger import Swagger
 
 app = Flask(__name__)
 
+swagger_config = Swagger.DEFAULT_CONFIG
+swagger_config['swagger_ui_bundle_js'] = '//unpkg.com/swagger-ui-dist@3/swagger-ui-bundle.js'
+swagger_config['swagger_ui_standalone_preset_js'] = '//unpkg.com/swagger-ui-dist@3/swagger-ui-standalone-preset.js'
+swagger_config['jquery_js'] = '//unpkg.com/jquery@2.2.4/dist/jquery.min.js'
+swagger_config['swagger_ui_css'] = '//unpkg.com/swagger-ui-dist@3/swagger-ui.css'
+Swagger(app, config=swagger_config)
+
 app.register_blueprint(api, url_prefix='/tracking')
+
+
