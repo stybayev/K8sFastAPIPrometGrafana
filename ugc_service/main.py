810daf6704e02b89352c5c63a437840652925b7e
dosxana@gmail.com <dosxana@gmail.com>
2024-08-09 11:07:14 +0500

    Добавил работу с jwt токенами при запросах

--- ugc_service/main.py
+++ ugc_service/main.py
@@ -7,6 +7,20 @@ from core.config import init_config
 
 app = Flask(__name__)
 
+# def init_redis():
+#     redis_conn = Redis(host=settings.redis_host, port=settings.redis_port)
+#     g.redis_conn = redis_conn
+#
+# @app.before_first_request
+# def initialize_resources():
+#     init_redis()
+#
+# @app.teardown_request
+# def close_resources(exception=None):
+#     redis_conn = g.pop('redis_conn', None)
+#     if redis_conn is not None:
+#         redis_conn.close()
+
 # Инициализируем конфигурацию приложения
 init_config(app)
 
