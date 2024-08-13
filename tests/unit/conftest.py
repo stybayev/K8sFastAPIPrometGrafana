87b9d82395bb187fc53107ce9bd33375980b474c
dosxana@gmail.com <dosxana@gmail.com>
2024-08-13 14:05:18 +0500

    Добавил тесты для ugc сервиса

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -6,4 +6,5 @@ pytest_plugins = [
     'tests.unit.fixtures.auth_service',
     'tests.unit.fixtures.mock_redis',
     'tests.unit.fixtures.auth_role_service',
+    'tests.unit.fixtures.ugc_services',
 ]
