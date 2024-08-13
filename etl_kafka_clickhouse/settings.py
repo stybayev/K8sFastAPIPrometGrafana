4135eb2bc0ad6164867aba57ae82bd61c6673c1f
Stybayev Doskhan <116162124+stybayev@users.noreply.github.com>
2024-08-13 13:32:06 +0500

    Update settings.py

--- etl_kafka_clickhouse/settings.py
+++ etl_kafka_clickhouse/settings.py
@@ -1,20 +1,4 @@
-# import os
-#
-# from pydantic_settings import BaseSettings
-#
-#
-# class Settings(BaseSettings):
-#     # clickhouse
-#     clickhouse_port: int = int(os.getenv('CLICKHOUSE_PORT'))
-#     clickhouse_poll_records: int = int(os.getenv('CLICKHOUSE_POLL_RECORDS'))
-#
-#     # kafka
-#     kafka_bootstrap_servers: list[str] = os.getenv('KAFKA_BOOTSTRAP_SERVERS').split(',')
-#     kafka_topics: list[str] = os.getenv('KAFKA_TOPICS').split(',')
-#     kafka_consumer_group_id: str = os.getenv('CONSUMER_GROUP_ID')
-#
-#
-# settings = Settings()
+
 from pydantic import ValidationError
 from pydantic_settings import BaseSettings
 from typing import List
