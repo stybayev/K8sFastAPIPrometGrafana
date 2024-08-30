008330ee15b4acdaa6f5ae8d68cc302809025326
kirill <domitin@gmail.com>
2024-08-30 20:54:59 +0300

    sentry for file_api

--- file_api/main.py
+++ file_api/main.py
@@ -1,10 +1,23 @@
+import os
 from contextlib import asynccontextmanager
-from miniopy_async import Minio
+
+import sentry_sdk
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
+from miniopy_async import Minio
+
 from file_api.api.v1 import files
 from file_api.core.config import settings
 from file_api.db.minio import set_minio, close_minio, create_bucket_if_not_exists
+from file_api.utils.sentry_hook import before_send
+
+sentry_sdk.init(
+    dsn=os.getenv("FILE_API_SENTRY_DSN"),
+    traces_sample_rate=1.0,
+    profiles_sample_rate=1.0,
+    send_default_pii=True,  # Включает передачу данных о пользователе
+    before_send=before_send,
+)
 
 
 @asynccontextmanager
